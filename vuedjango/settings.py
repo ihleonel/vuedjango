962d535359892a278d2eff795213b0800d3f48ea
Ibarra Hector Leonel <ibarra.h.leonel@gmail.com>
2020-08-05 19:58:10 -0300

    Correcciones en archivos docker y settings

--- vuedjango/settings.py
+++ vuedjango/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': config('DB_ENGINE'),
         'NAME': config('DB_NAME'),
-        'USER': config('DB_NAME'),
+        'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT')
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Salta'
 
 USE_I18N = True
 
