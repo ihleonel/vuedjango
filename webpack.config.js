2351460b69e764428bea405fc22736823a5c3284
Ibarra Hector Leonel <ibarra.h.leonel@gmail.com>
2020-08-08 21:43:43 -0300

    Configurando webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require('path')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index',
+
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist',
     filename: 'bundle.js'
   },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css'
+    })
+  ],
+
   module: {
     rules: [
       {
@@ -16,9 +25,16 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
         test: /\.scss$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader'
         ]
